742f001b67314513d98b90f8600c572b87c94175
Samy Pessé <samypesse@gmail.com>
2015-04-09 16:39:19 +0200

    Adapt for browserify

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-define([
-    "src/settings"
-], function(settings) {
-    var _ = codebox.require("hr/utils");
-    var commands = codebox.require("core/commands");
-    var rpc = codebox.require("core/rpc");
-    var dialogs = codebox.require("utils/dialogs");
+var settings = require("./settings");
 
-    // Commands
-    var runProject = commands.register({
-        id: "run.project",
-        title: "Run: Project",
-        icon: "playback-play",
-        shortcuts: [
-            "alt+r"
-        ],
-        run: function() {
-            runProject.set("icon", "playback-pause");
-        }
-    });
+var _ = codebox.require("hr.utils");
+var commands = codebox.require("core/commands");
+var rpc = codebox.require("core/rpc");
+var dialogs = codebox.require("utils/dialogs");
+
+// Commands
+var runProject = commands.register({
+    id: "run.project",
+    title: "Run: Project",
+    icon: "playback-play",
+    shortcuts: [
+        "alt+r"
+    ],
+    run: function() {
+        runProject.set("icon", "playback-pause");
+    }
 });
